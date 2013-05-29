b34c50a8babe06e099cf230d9a2ed6c091e000d8
feroz <feroz@feroz.(none)>
2013-05-30 00:40:44 +0530

    user interface and functional tests

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,6 +15,9 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '#columns #side a', :minimum => 4
+    assert_select 'h1', 'Listing products'
+    assert_select '#columns #main #product_list a', :minimum => 9
   end
 
   test "should get new" do
