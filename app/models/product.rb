01bd351dd0c7616d146b2d4da453b48bffbb6094
feroz <feroz@feroz.(none)>
2013-05-29 22:22:03 +0530

    unit testing and validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => { :with => %r{\.(gif|jpg|png)$}i, :message => 'must be url for gif, jpg, png image'}
+  validates :title, :length => {:minimum => 10, :message => "must be minimum 10 characters"}
 end
